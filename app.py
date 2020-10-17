425c67f9ac193700eb67fd4838329521a9bd6824
Elise Polo <70666985+elisepolo@users.noreply.github.com>
2020-10-17 11:38:58 -0400

    updated files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import requests
+print("requests file:', requests.__file__")
 
 app = Flask(__name__)
 
@@ -7,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/movies', methods=["POST"])
+def movies():
+    genre = request.form['genre']
+    r = requests.get("https://api.themoviedb.org/3/movie/"+genre+"?api_key=96c605a3f3793f8017e51b718cef1263")
+    json_object = r.text
+    return json_object
+    # return render_template('movies.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
