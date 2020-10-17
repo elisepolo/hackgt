72b6d3ad96c44e61ef7e3aabbd7b7359c6fff1a9
VarshC <49307186+VarshC@users.noreply.github.com>
2020-10-18 03:29:37 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, request
+import requests
+print("requests file:', requests.__file__")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/movies', methods=["POST"])
+def movies():
+    genre = request.form['genre']
+    if genre == 'Action':
+        genre = 18
+    r = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=4d00790bd9a1c4473522227ce0b3361b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + str(genre));
+    json_object = r.json()
+    movieName = json_object['results'][10]["title"]
+    return movieName
+    # return render_template('movies.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
